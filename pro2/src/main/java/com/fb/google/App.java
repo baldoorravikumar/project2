49843d2bf8cf58a0d363235b4713c9dec6211627
ravikumar <ravikumar.bld@gmail.com>
2018-06-07 16:48:01 +0530

    pro2

---
+++ pro2/src/main/java/com/fb/google/App.java
@@ -0,0 +1,13 @@
+package com.fb.google;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ravi,Welcome to DevOps!" );
+    }
+}
